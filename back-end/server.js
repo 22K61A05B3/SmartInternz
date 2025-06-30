e3e13b98e72b93ec1b59bfb3dcfc01eb81e8eb4a
Kiran Babu <n.kiranbabu630@gmail.com>
2025-06-30 22:46:59 +0530

    Initial commit

---
+++ back-end/server.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+const cors = require("cors");
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.use(cors());
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB connected successfully.");
+})
+.catch(()=>{
+    console.log("Error connecting to MongoDB.");
+});
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/auth", userRoutes);
+const port=process.env.PORT||5001;
+app.listen(port,()=>{
+    console.log("Server runnig on port http://localhost:"+port);
+});
